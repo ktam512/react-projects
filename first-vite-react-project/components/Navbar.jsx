2c2b5044e28186b31601a3241de14e7d531b2ce7
ktam512 <ktam512@gmail.com>
2023-08-22 00:12:14 +0700

    Adding the styles and flex display for the navigation bar

--- first-vite-react-project/components/Navbar.jsx
+++ first-vite-react-project/components/Navbar.jsx
@@ -1,5 +1,17 @@
+/**
+Challenge: Complete the Navbar to match the design
+
+Hint: use the Figma file for the most accurate peek at the design
+(colors, sizes, fonts, etc.)
+*/
 import React from "react"
 
 export default function Navbar() {
-    return <h1>Navbar component</h1>
+    return (
+        <nav>
+            <img src = "images/react.svg"/>
+            <h2>ReactFacts</h2>
+            <h3>React Course - Project 1</h3>
+        </nav>
+    )
 }
