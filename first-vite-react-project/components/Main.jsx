a0cd15b8431f2a9d1c1d354c7548f1c594a03e81
ktam512 <ktam512@gmail.com>
2023-08-22 19:40:15 +0700

    Add the basic component of the Main section of the website

--- first-vite-react-project/components/Main.jsx
+++ first-vite-react-project/components/Main.jsx
@@ -1,5 +1,18 @@
 import React from "react"
 
 export default function Main() {
-    return <h1>Main component</h1>
+    return (
+    <div className = "main">
+        <h1 className = "main--headline">Fun facts about React</h1>
+        <ul className = "main--list">
+            <li className = "main--list_element">Was first released in 2013</li>
+            <li className = "main--list_element">Was originally created by Jordan Walke</li>
+            <li className = "main--list_element">Has well over 100K stars on Github</li>
+            <li className = "main--list_element">Is mantained by Facebook</li>
+            <li className = "main--list_element">Powers thousands of enterprise apps, including moblie apps</li>
+        </ul>
+
+    </div>
+    )
+    
 }
