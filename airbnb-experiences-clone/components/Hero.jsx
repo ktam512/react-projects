99b631d6f1d109358a13bd3ef4063d6cff57c483
ktam512 <ktam512@gmail.com>
2023-09-04 18:18:13 +0700

    Making 50% of a Card model in the Card section Will comeback to finish it later.

--- airbnb-experiences-clone/components/Hero.jsx
+++ airbnb-experiences-clone/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Hero(){
     return (
-        <section>
+        <section className = "hero">
             <img src = "../images/photo-grid.png" className = "hero--image"/>
             <h1 className = "hero--title">Online Experiences</h1>
             <p className = "hero--text">Join unique interactive activities led by
